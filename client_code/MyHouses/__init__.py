0203b30c96e9942c956ca1cc8bdfc8f7c1234a84
wongibesahmoah@gmail.com <wongibesahmoah@gmail.com>
2024-05-13 14:07:56 +0000

    Created MyHouses; Edited settings, Base, Houses and Home

--- client_code/MyHouses/__init__.py
+++ client_code/MyHouses/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import HomeTemplate
+from ._anvil_designer import MyHousesTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
@@ -6,18 +6,11 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
-from ..Houses import Houses
 
 
-
-class Home(HomeTemplate):
+class MyHouses(MyHousesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-  def view_houses_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.content_panel.clear()
-    self.content_panel.add_component(Houses())
-
-   
+    # Any code you write here will run before the form opens.
