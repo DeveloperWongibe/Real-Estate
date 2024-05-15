ea411f8da5ad2a86fd65ea7cbacf2127a62daa62
wongibesahmoah@gmail.com <wongibesahmoah@gmail.com>
2024-05-15 10:22:49 +0000

    Edited Base

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -15,7 +15,7 @@ class Base(BaseTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.change_sign_in_text()
-    self.content_panel.add_component(Home())
+    self.go_to_home()
 
     # Any code you write here will run before the form opens.
   def change_sign_in_text(self):
@@ -25,18 +25,33 @@ class Base(BaseTemplate):
       self.sign_in.text = email
     else:
      self.sign_in.text = "SignIn"
+     self.toggle_my_courses_link()
+
+  def toggle_my_courses_link(self):
+    self.my_houses.visible = anvil.users.get_user() is not None
       
   def title_click(self, **event_args):
     """This method is called when the link is clicked"""
-    self.content_panel.clear()
-    self.content_panel.add_component(Home())
+    self.go_to_home()
 
   def my_houses_click(self, **event_args):
     """This method is called when the link is clicked"""
     self.content_panel.clear()
     self.content_panel.add_component(MyHouses())
 
+  def go_to_home(self):
+     self.content_panel.clear()
+     self.content_panel.add_component(Home())
+
   def sign_in_click(self, **event_args):
     """This method is called when the link is clicked"""
-    anvil.users.login_with_form()
-    self.change_sign_in_text()
+    user = anvil.users.get_user()
+    if user:
+      logout = confirm ("Would you like to logout?")
+      if logout:
+        anvil.users.logout()
+        self.go_to_home()
+      else:
+    
+        anvil.users.login_with_form()
+      self.change_sign_in_text()
